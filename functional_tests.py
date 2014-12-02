a58ff7eca9b34f45746d56cf9aaa84180b7b2290
Ognjen Glišić <ognjen@inbox.com>
2014-12-02 13:22:06 +0100

    Functional test now checks we can input an invitation message

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitortest(unittest.TestCase):
@@ -19,23 +20,37 @@ class NewVisitortest(unittest.TestCase):
         # Primjecuje da naslov stranice i zaglavlje spominju
         # sportskih aktivnosti.
         self.assertIn('sportske aktivnosti', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('sportske aktivnosti', header_text)
         
         # Pozvan je da napise pozivnicu za svoju prvu aktivnost
-
+        inputbox = self.browser.find_element_by_id('id_new_message')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter an invitation message'
+        )
+        
         # Upisuju "Termin 10.01.! Hajmo prijave! :)" u text box 
+        inputbox.send_keys('Termin 10.01.! Hajmo prijave! :)')
 
-        # Kada pritisne enter, stranica se azurira i prizuje se pozivnica
+        # Kada pritisne enter, stranica se azurira i prikazuje se pozivnica
         # koju je upravo napisao "Termin 10.01.! Hajmo prijave! :)"
+        inputbox.send_keys(Keys.ENTER)
+        comments_list = self.browser.find_element_by_id('id_ul_comments')
+        comments = comments_list.find_elements_by_tag('li')
+        self.assertTrue(
+            any(comment.text == 'Termin 10.01.! Hajmo prijave! :)' for comment in comments)
+        )        
 
         # Tu se i dalje nalazi text box koji poziva da napise odgovor na pozivnicu
-        # ili dodatni komentar. On upisuje "Žarko" da bi drugi znali da je on 
+        # ili dodatni komentar. On upisuje "Zarko" da bi drugi znali da je on 
         # napisao pozivnicu.
+        self.fail('Finish test')
 
         # Stranica se ponovo osvježava i sada se ispod postojeće poruke vidi drugi
         # komentar.
 
-        # Žarko se pita da li ce aplikacija zapamtiti njegovu pozivnicu i da li ce
+        # Zarko se pita da li ce aplikacija zapamtiti njegovu pozivnicu i da li ce
         # je moci poslati prijateljima na Facebook ili na mail. Tada primjecuje da
         # je aplikacija generisala jedinstveni URL za njegovu pozivnicu.
 
